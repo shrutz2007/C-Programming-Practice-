6e157acf95fb65e4788059d7236ca875a95091ee
Shruti Jawale <66550110+shrutz2007@users.noreply.github.com>
2021-02-07 17:09:14 +0530

    Add files via upload

---
+++ 23#Smily faces using for loop.c
@@ -0,0 +1,14 @@
+/*Smily faces using for loop*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char i,j;
+    for(i=1;i<=24;i++)
+    {
+        for(j=1;j<=79;j++)
+        {
+            printf("%c",1);
+        }
+    }
+}
